d333cc2f66abfa8a0965a5f370e96a3d5290aa0d
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-02 07:45:33 +0100

    changed file name

--- Day 1/Day_1.py
+++
@@ -1,3 +0,0 @@
-lines = list(map(int, open('input.txt', 'r').readlines()))
-print(sum(1 for i in range(len(lines)) if lines[i-1] < lines[i]))  # part 1
-print(sum(1 for i in range(len(lines)) if lines[i-3] < lines[i]))  # part 2
