617670a2381ad957d74e2596105ba1b9f2dc4f36
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-02 09:19:26 +0100

    clean code a bit

--- Day_2/Day_2.py
+++ Day_2/Day_2.py
@@ -1,7 +1,7 @@
 lines = [[j[0], int(j[1])] for j in map(lambda i: i.split(), open('input.txt', 'r').readlines())]
 
 # Part 1
-depth, hor, aim = 0, 0, 0
+depth, hor = 0, 0
 for pos, x in lines:
     hor += x * (pos == "forward")
     depth += x * (pos == "down") or -(x * (pos == "up"))
