b88df5ceb59a07cb01b033601304da4c9a6e7051
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-03 12:31:47 +0100

    Added puzzle instructions to folders

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -9,10 +9,17 @@ for i in range(len(lines[0])):
 print(int(gamma, 2) * int(ep, 2))
 
 # Part 2
-ox, co = lines[:], lines
+ox, co = lines, lines
 for i in range(len(lines[0])):
     ox_ = sum(int(_[i]) for _ in ox) - len(ox)/2
     co_ = sum(int(_[i]) for _ in co) - len(co)/2
     ox = [x for x in ox if x[i] == str(int(ox_ >= 0))]
     co = [x for x in co if x[i] == str(int(co_ < 0)) or len(co) < 2]
 print(int(ox[0], 2) * int(co[0], 2))
+
+"""Explanation:
+ox_ and co_ are sums of the total 1's per column of bytes (top to bottom), 
+it is than subtracted with half the length of the column, if there are more 1's than 0's
+the end result should give a number above 0, else below 0. This outcome is then applied in the
+lines below. If the bit is a one and the statement is true (there where more ones), than that
+byte stays in the list."""
