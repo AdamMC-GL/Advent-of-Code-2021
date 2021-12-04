96e46cc4b2960664656a6813fdbb5b57180413c5
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-04 14:11:32 +0100

    day 4 done

--- Day_4/Day_4.py
+++ Day_4/Day_4.py
@@ -1,1 +1,36 @@
-lines = open('input.txt', 'r').read().splitlines()
+lines = list(open('input.txt', 'r').read().split('\n\n'))
+numbers = list(map(int, lines[0].split(',')))
+boards = [[list(map(int, k.split())) for k in j] for j in [i.split('\n') for i in lines[1:]]]
+
+def part1():
+    for i in numbers:
+        for board in boards:
+            for row in board:
+                for l in range(len(row)):
+                    if i == row[l]:
+                        row[l] = 0
+                    if sum(row) == 0 or sum(board[x][l] for x in range(5)) == 0:
+                        print(sum(sum(row) for row in board) * i)
+                        return
+
+
+def part2():
+    for i in numbers:
+        j = 0
+        while j < len(boards):
+            for row in boards[j]:
+                for l in range(len(row)):
+                    if i == row[l]:
+                        row[l] = 0
+                    if sum(row) == 0 or sum(boards[j][x][l] for x in range(5)) == 0:
+                        if len(boards) < 2:
+                            print(sum(sum(row) for row in boards[0]) * i)
+                            return
+                        boards.pop(j)
+                        j -= 1
+            j += 1
+
+
+part1()
+part2()
+
