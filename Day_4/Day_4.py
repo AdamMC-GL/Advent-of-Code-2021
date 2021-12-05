2df8997c002ab9465fd1b268b2f24979f5ba638c
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-05 13:47:15 +0100

    day 5 done

--- Day_4/Day_4.py
+++ Day_4/Day_4.py
@@ -2,14 +2,15 @@ lines = list(open('input.txt', 'r').read().split('\n\n'))
 numbers = list(map(int, lines[0].split(',')))
 boards = [[list(map(int, k.split())) for k in j] for j in [i.split('\n') for i in lines[1:]]]
 
+
 def part1():
     for i in numbers:
         for board in boards:
             for row in board:
-                for l in range(len(row)):
-                    if i == row[l]:
-                        row[l] = 0
-                    if sum(row) == 0 or sum(board[x][l] for x in range(5)) == 0:
+                for j in range(len(row)):
+                    if i == row[j]:
+                        row[j] = 0
+                    if sum(row) == 0 or sum(board[x][j] for x in range(5)) == 0:
                         print(sum(sum(row) for row in board) * i)
                         return
 
@@ -19,10 +20,10 @@ def part2():
         j = 0
         while j < len(boards):
             for row in boards[j]:
-                for l in range(len(row)):
-                    if i == row[l]:
-                        row[l] = 0
-                    if sum(row) == 0 or sum(boards[j][x][l] for x in range(5)) == 0:
+                for k in range(len(row)):
+                    if i == row[k]:
+                        row[k] = 0
+                    if sum(row) == 0 or sum(boards[j][x][k] for x in range(5)) == 0:
                         if len(boards) < 2:
                             print(sum(sum(row) for row in boards[0]) * i)
                             return
@@ -33,4 +34,3 @@ def part2():
 
 part1()
 part2()
-
