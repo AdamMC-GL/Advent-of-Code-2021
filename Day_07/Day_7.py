28a00326ff95a98afed8994521fda02902c83cc9
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2021-12-07 07:36:37 +0100

    update day 6, 7 day done

--- Day_07/Day_7.py
+++ Day_07/Day_7.py
@@ -1,1 +1,22 @@
 lines = list(map(int, open('input.txt', 'r').read().split(',')))
+
+# Part 1
+n = min(lines)
+old, new = 1, 0
+while new < old or old == 0:
+    n += 1
+    old = new
+    new = sum(abs(n-j) for j in lines)
+print(n, int(old))
+
+# Part 2
+n = min(lines)
+old, new = 1, 0
+while new < old or old == 0:
+    n += 1
+    old = new
+    new = sum((abs(n - j) + 1)*(abs(n - j))/2 for j in lines)
+print(n, int(old))
+
+
+
